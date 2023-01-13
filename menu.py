1faaed9e2ea3b1cb96cc2567e39e439c00d8fccf
HoroT <kazanskijdaniil47@gmail.com>
2023-01-14 00:32:22 +0300

    2

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import pygame
-#from game_screen import Cursor
+
+# from game_screen import Cursor
 
 pygame.display.init()
 img = pygame.image.load('картинки/fon_menu.jpg')
@@ -10,9 +11,8 @@ all_sprites = pygame.sprite.Group()
 sprite = pygame.sprite.Sprite()
 
 
-
 class new(pygame.sprite.Sprite):
-    image = pygame.image.load("картинки/cursor.png")
+    image = pygame.image.load("картинки/game.png")
 
     def __init__(self, *group):
         super().__init__(*group)
@@ -23,7 +23,7 @@ class new(pygame.sprite.Sprite):
 
 
 class reg(pygame.sprite.Sprite):
-    image = pygame.image.load("картинки/interface.jpg")
+    image = pygame.image.load("картинки/fon_menu.jpg")
 
     def __init__(self, *group):
         super().__init__(*group)
@@ -33,32 +33,32 @@ class reg(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = w // 2 - 200, h // 3
 
 
-class setting(pygame.sprite.Sprite):
-    image = pygame.image.load("картинки/interface.jpg")
+'''class sett(pygame.sprite.Sprite):
+    image = pygame.image.load("картинки/game.png")
 
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = reg.image
+        self.image = sett.image
+        self.image = pygame.transform.scale(self.image, (400, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = w // 2 - 200, h // 3 + 150'''
+
+
+class ex(pygame.sprite.Sprite):
+    image = pygame.image.load("картинки/ext.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = ex.image
         self.image = pygame.transform.scale(self.image, (400, 50))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = w // 2 - 200, h // 3 + 150
 
 
-class ex(pygame.sprite.Sprite):
-    image = pygame.image.load("картинки/ex.png")
-
-    def __init__(self, *group):
-        super().__init__(*group)
-        self.image = reg.image
-        self.image = pygame.transform.scale(self.image, (400, 50))
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = w // 3 * 2, h // 3 * 2
-
-
 running = True
 new(all_sprites)
 reg(all_sprites)
-setting(all_sprites)
+# sett(all_sprites)
 ex(all_sprites)
 pos = None
 while running:
@@ -74,7 +74,6 @@ while running:
             if (w // 27) < pos[0] < (w // 27) + (w // 5):
                 if (h // 171) < pos[1] < (h // 171) + (h // 26):
                     running = False
-
 
     pygame.display.flip()
 pygame.quit()
